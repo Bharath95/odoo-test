7f247a0b5fb27408cf46ca44330abbfb5d7b52b5
Bharath95 <o.bharath95@gmail.com>
2025-04-14 21:04:36 +0530

    workflow testing

---
+++ custom-addons/test_module/models/syntax_error.py
@@ -0,0 +1,6 @@
+def test_function():
+    print("This is a syntax error")
+    print("Another line")
+    print("One more line")
+    print("Final line")
+    print("This line has no newline at the end")
